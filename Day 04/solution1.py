968b1719d7bd1c65a47bb17a41042a3dc1835dd2
jt17819 <jt17819@alumni.bristol.ac.uk>
2025-12-04 10:30:37 +0000

    Completed Day 4

---
+++ Day 04/solution1.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def main():
+    with open("Day 04/data.txt", "r") as file:
+        data = file.read().split("\n")
+    data = np.array([[char for char in line] for line in data])
+    # print(data)
+
+    total = 0
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            if data[row][col] == "@":
+                total += count_neighbours(row, col, data) < 4
+
+    return total
+
+
+def count_neighbours(y, x, grid):
+    max_y, max_x = len(grid) - 1, len(grid[0]) - 1
+    top = y == 0
+    bottom = y == max_y
+    left = x == 0
+    right = x == max_x
+    count = 0
+
+    if not top:
+        count += grid[y - 1][x] == "@"
+        if not left:
+            count += grid[y - 1][x - 1] == "@"
+        if not right:
+            count += grid[y - 1][x + 1] == "@"
+    if not bottom:
+        count += grid[y + 1][x] == "@"
+        if not left:
+            count += grid[y + 1][x - 1] == "@"
+        if not right:
+            count += grid[y + 1][x + 1] == "@"
+    if not left:
+        count += grid[y][x - 1] == "@"
+    if not right:
+        count += grid[y][x + 1] == "@"
+
+    return count
+
+
+if __name__ == "__main__":
+    print(main())
